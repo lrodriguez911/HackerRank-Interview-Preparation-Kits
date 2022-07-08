0575f585ec9e0f216330b49976eee80d561be18b
lucasr <lrodriguez.arevalosc@gmail.com>
2022-07-08 11:44:02 -0300

    add exercises to hackerrank

---
+++ Mini-Max Sum/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function miniMaxSum(arr) {
+    // Write your code here
+    let arrOrd = arr.sort((a,b) => a - b)
+    let minSum = arrOrd.filter(a => a !== arrOrd[4])
+    .reduce((a,b) =>a + b ,0)
+    let maxSum = arrOrd.filter(a => a !== arrOrd[0])
+    .reduce((a,b) =>a + b,0)
+
+arrOrd[0] === arrOrd[4] && arrOrd[0] > 0 ? 
+console.log(arrOrd[0] * 4,arrOrd[0] * 4): 
+console.log(minSum, maxSum)
+}
+
+function main() {
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    miniMaxSum(arr);
+}
---
+++ Time Conversion/index.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+    // Write your code here
+let AMoPM = s.slice(-2)
+console.log(AMoPM)
+console.log(parseInt(s.slice(0,-2).split(':')[0]))
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const result = timeConversion(s);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
+timeConversion('07:05:45PM')
